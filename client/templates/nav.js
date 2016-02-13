f201010ef26e1561f6d1cfcdc5c86dcbc2d5dbd3
rsata <reid.t.sata@gmail.com>
2016-02-12 17:09:57 -0800

    nav and spacing

--- client/templates/nav.js
+++ client/templates/nav.js
@@ -11,9 +11,17 @@ Template.nav.events({
     if (nav.hasClass('hide')) {
       nav.removeClass('hide');
       nav.addClass('show');
+      $('#menuX').removeClass('hide');
+      $('#menuX').addClass('show');
+      $('#menuLines').removeClass('show');
+      $('#menuLines').addClass('hide');
     } else {
       nav.removeClass('show');
       nav.addClass('hide');
+      $('#menuX').removeClass('show');     
+      $('#menuX').addClass('hide'); 
+      $('#menuLines').removeClass('hide');
+      $('#menuLines').addClass('show');
     }
 
   }
